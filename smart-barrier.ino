bd58fe4c42d76b906876cceea161031eaa4c7dee
Henry Munachimso <84022403+Draycole@users.noreply.github.com>
2025-08-21 06:36:44 +0100

    Update smart-barrier.ino

--- smart-barrier.ino
+++ smart-barrier.ino
@@ -3,7 +3,7 @@
 #define trigPin 9     
 #define echoPin 10
 
-int leds[2] = {5,6};  //open led is on pin 5, closed is on pin 6. 
+int leds[2] = {5,6};  //Open LED is on pin 5, Closed is on pin 6. 
 
 const int buzzer = 11; 
 
@@ -15,7 +15,7 @@ void setup() {
   pinMode(trigPin, OUTPUT);   
   pinMode(echoPin, INPUT);    
   
-  myServo.attach(3);    // Define Servo Pin
+  myServo.attach(3);   
   for (int i = 0; i < 2; i++){    //Define LED pins as output
     pinMode(leds[i], OUTPUT);
   }
@@ -24,7 +24,7 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(trigPin, LOW);   // Clear the trigger pin
+  digitalWrite(trigPin, LOW);  
   delayMicroseconds(2);
 
   // send a 10us HIGH pulse to the trigger pin
@@ -35,7 +35,7 @@ void loop() {
   // Measure echo time
   long duration = pulseIn(echoPin, HIGH);
 
-  // Convert duration to dictance in cm assuming the speed of sound assumed to be 340m/s
+  // Convert duration to distance in cm assuming the speed of sound assumed to be 340m/s
   long distance = duration * 0.034 / 2;
 
   Serial.print("Distance: ");
@@ -50,7 +50,6 @@ void loop() {
   //Set gate flat
   myServo.write(90);
 
-  //light up based on distance
   if (distance > 14) {
     digitalWrite(leds[1], HIGH); // Farther than 14 cm set servo to close the barrier
     myServo.write(90);      
@@ -61,7 +60,7 @@ void loop() {
 
     //Buzzer tone
     for (int i=0; i<10; i++) {
-        tone(buzzer, 1000); // send 1KHz sound signal
+        tone(buzzer, 1000);
         delay(100);        
         noTone(buzzer);     
         delay(100);        
